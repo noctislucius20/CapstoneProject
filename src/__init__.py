b10e71b3491eb55d51547481622a5fe49516381f
dharma_k <noctislucius20@gmail.com>
2022-05-16 16:26:39 +0700

    add db migration

--- src/__init__.py
+++ src/__init__.py
@@ -2,24 +2,29 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 
+from flask_migrate import Migrate
+
+DB_NAME = "capstone"
 db = SQLAlchemy()
-DB_NAME = "capstone.db"
 
 def create_app():
     app = Flask(__name__)
+
     app.config['SECRET_KEY'] = '0d6d55a89139aa5e2b1640f87a1f4abb'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://localhost/{DB_NAME}?user=postgres&password=scipio'
     db.init_app(app)
+
+    migrate = Migrate(app, db)
 
     from controllers.User import user
 
     app.register_blueprint(user, url_prefix='/')
 
-    create_database(app)
+    # create_database(app)
 
     return app
 
-def create_database(app):
-    if not path.exists('src/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')  
+# def create_database(app):
+#     if not path.exists('src/' + DB_NAME):
+#         db.create_all(app=app)
+#         print('Created Database!')  
