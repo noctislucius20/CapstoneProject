6ef7dea8c253dc1990cb5e4c4a50090b99118bdf
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-05-17 15:54:53 +0700

    user login

--- controllers/User.py
+++ controllers/User.py
@@ -1,11 +1,32 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, make_response
 from nanoid import generate
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from models.User import User
 from src import db
+from functools import wraps
 
+import jwt
+import datetime
 
 user = Blueprint('user', __name__)
+
+# decorator as middleware
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return make_response(jsonify({'msg': 'Token is missing!'}), 401)
+        
+        # decode token
+        try:
+            output = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except:
+            return make_response(jsonify({'msg': 'Token is invalid!'}), 401)
+        
+        return f(*args, **kwargs)
+    
+    return decorator
 
 @user.route('/users', methods=['POST'])
 def create_user():
@@ -19,7 +40,23 @@ def create_user():
 
     return jsonify({"message": "New user created"})
 
+@user.route('/users/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    logged_in = check_password_hash(user.password, data['password'])
+    # check password user
+    if not logged_in:
+        return jsonify({"message": "Wrong password"}), 401
+    
+    token = jwt.encode({'username': data['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, 'secret', algorithm='HS256')
+    return make_response(jsonify({'token': token}))
+
 @user.route('/users/<username>', methods=['GET'])
+@token_required
 def get_one_user(username):
     user = User.query.filter_by(username=username).first()
 
--- models/User.py
+++ models/User.py
@@ -5,7 +5,7 @@ from src import db
 
 class User(db.Model):
     id = db.Column(db.String(50), primary_key=True)
-    username = db.Column(db.String(30), unique=true, nullable=False)
+    username = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
-    full_name = db.Column(db.String(100), nullable=False) 
+    full_name = db.Column(db.String(100), nullable=False)
 
