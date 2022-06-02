791fcd8ded327bbd17c6a18f01c2f34b59b129a1
dharma_k <noctislucius20@gmail.com>
2022-06-02 17:15:33 +0700

    change host and local port

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from src import create_app
-from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
+    app.run(debug=True, port=5001)
