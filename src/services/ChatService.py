941e5264f0562b7f7cfda6a16a7b1090a063344e
dharma_k <noctislucius20@gmail.com>
2022-06-08 17:48:33 +0700

    change ml url

--- src/services/ChatService.py
+++ src/services/ChatService.py
@@ -5,9 +5,8 @@ import urllib.request
 class ChatService:
     def get_predicted_chat(self, data):
         data_to_bytes = json.dumps(data).encode('utf-8')
-        host = os.getenv('ML_SERVER_HOST')
-        port = os.getenv('ML_SERVER_PORT')
-        url = f'http://{host}:{port}/smart_chatbot/predict'
+        ml_url = os.getenv('ML_SERVER_URL')
+        url = f'https://{ml_url}/smart_chatbot/predict'
         req = urllib.request.Request(url, method='GET')
         req.add_header('Content-Type', 'application/json')
         returned_data = urllib.request.urlopen(req, data_to_bytes)
